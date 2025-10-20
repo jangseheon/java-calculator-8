25cc2eb960de976b59fcde8c143aab907f20e460
Jang seheon <tpgjs3121@naver.com>
2025-10-20 23:22:40 +0900

    feat: 에러 던진 뒤 프로그램 종료를 위한 코드 분리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,28 +4,32 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            String input = Console.readLine();
-            String regex = "[,:]";
-            int result = 0;
-            if (input.equals("")) {
-                System.out.println("결과 : 0");
-                return;
-            }
-            if (input.startsWith("//")) {
-                if(input.indexOf("\n") != 3) throw new IllegalArgumentException("커스텀 구분자는 하나만 가능합니다.");
-                regex = String.valueOf(input.charAt(2));
-                input = input.substring(4);
-            }
-            String[] tokens = input.split(regex);
-            for (String s : tokens) {
-                if (!(s.charAt(0) >= '1' && s.charAt(0) <= '9'))
-                    throw new IllegalArgumentException("양수가 아닌 값은 들어갈 수 없습니다.");
-                result += Integer.parseInt(s);
-            }
-            System.out.printf("결과 : " + result);
-        }catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
+        String input = Console.readLine();
+
+        if (input.equals("")) {
+            System.out.println("결과 : 0");
+            return;
         }
+
+        System.out.printf("결과 : " + add(input));
+    }
+
+    static int add(String input){
+        int result = 0;
+        String regex = "[,:]";
+
+
+        if (input.startsWith("//")) {
+            if(input.indexOf("\n") != 3) throw new IllegalArgumentException("커스텀 구분자는 하나만 가능합니다.");
+            regex = String.valueOf(input.charAt(2));
+            input = input.substring(4);
+        }
+        String[] tokens = input.split(regex);
+        for (String s : tokens) {
+            if (!(s.charAt(0) >= '1' && s.charAt(0) <= '9'))
+                throw new IllegalArgumentException("양수가 아닌 값은 들어갈 수 없습니다.");
+            result += Integer.parseInt(s);
+        }
+        return result;
     }
 }
