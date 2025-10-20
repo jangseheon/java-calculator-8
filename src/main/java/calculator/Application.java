5156ccab27f0eaa8d67252983f5bd0f205882ce5
Jang seheon <tpgjs3121@naver.com>
2025-10-20 23:04:59 +0900

    feat: 유효한 문자열 입력에 대해 기본 구분자 적용기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,9 +5,12 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         String input = Console.readLine();
+        String regex = "[,:]";
         if(input.equals("")){
             System.out.println("결과 : 0");
             return;
         }
+
+        String[] tokens = input.split(regex);
     }
 }
