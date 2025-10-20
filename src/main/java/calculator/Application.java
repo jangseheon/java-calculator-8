1cda2a5c1e1f4dadf5133cd85cfb746e233c23f0
Jang seheon <tpgjs3121@naver.com>
2025-10-20 23:07:54 +0900

    feat: 입력한 문자열을 정수화하여 모두 더한 뒤 결과 출력 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,6 +6,7 @@ public class Application {
     public static void main(String[] args) {
         String input = Console.readLine();
         String regex = "[,:]";
+        int result = 0;
         if(input.equals("")){
             System.out.println("결과 : 0");
             return;
@@ -15,5 +16,9 @@ public class Application {
             input = input.substring(4);
         }
         String[] tokens = input.split(regex);
+        for (String s : tokens) {
+            result += Integer.parseInt(s);
+        }
+        System.out.printf("결과 : " + result);
     }
 }
