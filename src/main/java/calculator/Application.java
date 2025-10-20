de2648619db6f35db4e51f0931d75f1e016c8ea0
Jang seheon <tpgjs3121@naver.com>
2025-10-20 23:10:12 +0900

    문자열을 나눴을 때 양수 값이 아닌 것이 있을 때 오류발생 로직 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,21 +4,27 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        String input = Console.readLine();
-        String regex = "[,:]";
-        int result = 0;
-        if(input.equals("")){
-            System.out.println("결과 : 0");
-            return;
+        try {
+            String input = Console.readLine();
+            String regex = "[,:]";
+            int result = 0;
+            if (input.equals("")) {
+                System.out.println("결과 : 0");
+                return;
+            }
+            if (input.startsWith("//")) {
+                regex = String.valueOf(input.charAt(2));
+                input = input.substring(4);
+            }
+            String[] tokens = input.split(regex);
+            for (String s : tokens) {
+                if (!(s.charAt(0) >= '1' && s.charAt(0) <= '9'))
+                    throw new IllegalArgumentException("양수가 아닌 값은 들어갈 수 없습니다.");
+                result += Integer.parseInt(s);
+            }
+            System.out.printf("결과 : " + result);
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
         }
-        if(input.startsWith("//")){
-            regex = String.valueOf(input.charAt(2));
-            input = input.substring(4);
-        }
-        String[] tokens = input.split(regex);
-        for (String s : tokens) {
-            result += Integer.parseInt(s);
-        }
-        System.out.printf("결과 : " + result);
     }
 }
